85f36629d8ed125690ccd7d2ce5d5ed32d3a61ce
LynnAnEar <LynnAnEar@proton.me>
2026-07-23 00:35:44 +0000

    Refactor Magisk installer for font management
    Updated the Magisk installer script to streamline font installation and family entry injection.

--- customize.sh
+++ customize.sh
@@ -1,69 +1,64 @@
 #!/system/bin/sh
-##########################################################################################
-# Config Flags
-##########################################################################################
+# Simple Magisk installer: installs bundled fonts (fonts/) and injects family entries.
 
-# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
-# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment DEBUG if you want full debug logs (saved to /sdcard)
-MINAPI=29
-#MAXAPI=25
-#DYNLIB=true
-#DEBUG=true
+set -e
+ui_print() { echo "$@"; }
 
-##########################################################################################
-# Replace list
-##########################################################################################
+mkdir -p $MODPATH/system/fonts
+mkdir -p $MODPATH/system/etc
 
-# List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
+TMPDIR=${TMPDIR:-/data/local/tmp}
 
-# Construct your list in the following format
-# This is an example
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here
-REPLACE="
-"
-
-##########################################################################################
-# Permissions
-##########################################################################################
-
-set_permissions() {
-  :  # Remove this if adding to this function
-
-  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
-  # Some examples:
-  
-  # For directories (includes files in them):
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
-  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
-
-  # For files (not in directories taken care of above)
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
-  # set_perm /data/local/tmp/file.txt 0 0 644
-}
-
-SKIPUNZIP=1
-unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
-. $TMPDIR/functions.sh
-
-OVERLAY_IMAGE_EXTRA=0     # number of kb need to be added to overlay.img
-OVERLAY_IMAGE_SHRINK=true # shrink overlay.img or not?
-
-if [ -f "/data/adb/modules/magisk_overlayfs/util_functions.sh" ] && \
-    /data/adb/modules/magisk_overlayfs/overlayfs_system --test; then
-  ui_print "- Add support for overlayfs"
-  . /data/adb/modules/magisk_overlayfs/util_functions.sh
-  support_overlayfs && rm -rf "$MODPATH"/system
+# 1) extract bundled fonts/ from the zip (if present)
+unzip -qjo "$ZIPFILE" 'fonts/*' -d $TMPDIR || true
+if [ -d "$TMPDIR/fonts" ] && [ "$(ls -A $TMPDIR/fonts 2>/dev/null)" ]; then
+  ui_print "- Copying bundled font(s)..."
+  cp -a "$TMPDIR/fonts/"* "$MODPATH/system/fonts/" 2>/dev/null || true
 fi
+
+# 2) prepare family additions (common/my_families.xml)
+unzip -qjo "$ZIPFILE" 'common/my_families.xml' -d $TMPDIR || true
+if [ -f "$TMPDIR/my_families.xml" ]; then
+  FAM_FILE="$TMPDIR/my_families.xml"
+else
+  cat > "$TMPDIR/my_families.xml" <<'EOF'
+<family>
+  <name>Fredoka</name>
+  <font weight="400" style="normal">/system/fonts/Fredoka-Regular.ttf</font>
+</family>
+<alias name="sans-serif" to="Fredoka" />
+EOF
+  FAM_FILE="$TMPDIR/my_families.xml"
+fi
+
+# 3) patch system fonts.xml safely by inserting our families before </familyset>
+SYS_FONTS_XML=/system/etc/fonts.xml
+if [ -f "$SYS_FONTS_XML" ]; then
+  ui_print "- Creating overlayed fonts.xml at $MODPATH/system/etc/fonts.xml (inserting Fredoka family)..."
+  cp -f "$SYS_FONTS_XML" "$TMPDIR/orig_fonts.xml"
+  OUT="$MODPATH/system/etc/fonts.xml"
+  awk -v add="$(sed 's/\\/\\\\/g; s/"/\\"/g; s/$/\\n/' "$FAM_FILE" | tr -d '\r' | sed ':a;N;$!ba;s/\n/\\n/g')" '
+  {
+    if ($0 ~ /<\/familyset>/ && !done) {
+      split(add, a, "\\n")
+      for (i=1;i<=length(a);i++) if (a[i] != "") print a[i]
+      done=1
+    }
+    print $0
+  }' "$TMPDIR/orig_fonts.xml" > "$OUT" || {
+    ui_print "  ! Failed to inject into fonts.xml - copying original to overlay instead."
+    cp -f "$TMPDIR/orig_fonts.xml" "$OUT"
+  }
+else
+  ui_print "  ! /system/etc/fonts.xml not found; skipping automatic patch. Add a fonts.xml in common/my_families.xml if needed."
+fi
+
+# 4) set permissions
+ui_print "- Setting permissions..."
+set_perm_recursive $MODPATH/system/fonts 0 0 0755 0644
+if [ -f "$MODPATH/system/etc/fonts.xml" ]; then
+  set_perm $MODPATH/system/etc/fonts.xml 0 0 0644
+fi
+
+ui_print "- Fredoka install prepared. Reboot to apply fonts."
+exit 0
